78958f262c01e3f16e1e18be03a2ccf2210eeaf8
Philip Murray <philipmurray2009@gmail.com>
2023-07-16 20:35:18 +0100

    Included License
    Included License

--- EmailValidation/Form1.cs
+++ EmailValidation/Form1.cs
@@ -1,31 +1,8 @@
-﻿/*
- *   This file is part of EmailValidation source code.  All Rights Reserved.
- *
- *  Serial-Comm-Tetster is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License.
- *
- *  This software is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this software; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
- *  USA.
- */
-
-
-/*
- *     Author: Philip Murray
- *     Project Homepage: https://github.com/PhilipMur/EmailValidation
- */
-
+﻿
 /*
  *   This file is part of EmailValidation source code.  All Rights Reserved.
  *
- *  Serial-Comm-Tetster is free software; you can redistribute it and/or modify
+ *  EmailValidation is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License.
  *
