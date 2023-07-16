748377de8ae4779a4ee70c30d94f2918a7d7dc14
Philip Murray <philipmurray2009@gmail.com>
2023-07-16 20:50:12 +0100

    Update AssemblyInfo.cs

--- EmailValidation/Properties/AssemblyInfo.cs
+++ EmailValidation/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EmailValidation")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright Philip Murray ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
